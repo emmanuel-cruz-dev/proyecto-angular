6bfa436cd7a84a8cdf389302b0ff8806c9fb4a3e
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-21 19:36:30 -0300

    manejando contact component ts

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  contactForm?: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.contactForm = this._formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zip: ['', Validators.required],
+      agreeTerms: [false, Validators.requiredTrue],
+    });
+  }
+
+  ngOnInit(): void {}
+
   enviar(event: Event) {
     event.preventDefault();
     console.log('Enviado');
