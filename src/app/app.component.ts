567dfb8f8f75b5b6744258a66c5a5b3558818484
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-21 15:23:55 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'landingPageAngular';
+  menuOption: string = '';
+
+  onOption(menuOption: string) {
+    this.menuOption = menuOption;
+  }
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private _http = inject(HttpClient);
+  private urlBase: string = 'https://fakestoreapi.com/products';
+
+  getProducts(): Observable<any> {
+    return this._http.get<any>(this.urlBase);
+  }
+
+  getProduct(id: number): Observable<any> {
+    return this._http.get<any>(`${this.urlBase}/${id}`);
+  }
+}
