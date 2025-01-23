0719bb7a931d57902ecdf5517f427afa49f74078
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-23 15:06:43 -0300

    creando y agregando footer al flujo principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgClass, RouterLink],
+  imports: [RouterOutlet, NgClass, RouterLink, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
