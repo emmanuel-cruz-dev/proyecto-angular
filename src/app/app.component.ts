9c2e7f05a25132cd318bda34747f648fb8bea3c0
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-31 17:07:04 -0300

    implementando backdrop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Renderer2, ElementRef } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 
@@ -9,10 +9,44 @@ import { FooterComponent } from './footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   menuOption: string = '';
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   onOption(menuOption: string) {
     this.menuOption = menuOption;
+  }
+
+  ngAfterViewInit() {
+    const headerEls = this.el.nativeElement.querySelectorAll(
+      '.collapse .nav-item'
+    );
+    const menuBackdrop = this.el.nativeElement.querySelector('#menu-backdrop');
+
+    headerEls.forEach((el: HTMLElement) => {
+      this.renderer.listen(el, 'click', () => {
+        this.renderer.setStyle(
+          menuBackdrop,
+          '--backdrop-top',
+          `${el.offsetTop}px`
+        );
+        this.renderer.setStyle(
+          menuBackdrop,
+          '--backdrop-left',
+          `${el.offsetLeft}px`
+        );
+        this.renderer.setStyle(
+          menuBackdrop,
+          '--backdrop-width',
+          `${el.offsetWidth}px`
+        );
+        this.renderer.setStyle(
+          menuBackdrop,
+          '--backdrop-height',
+          `${el.offsetHeight}px`
+        );
+      });
+    });
   }
 }
